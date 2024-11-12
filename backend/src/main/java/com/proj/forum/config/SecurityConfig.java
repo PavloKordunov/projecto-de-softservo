e99d761ccb3efe58f3e6f477e77f8774401f4d3a
gontar290 <roman.hontar.pm.2023@lpnu.ua>
2024-11-12 17:59:43 +0200

    add login logic and home controller

---
+++ backend/src/main/java/com/proj/forum/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.proj.forum.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests(authorize -> authorize.
+                        anyRequest().authenticated()
+                ).oauth2Login(withDefaults());
+        return http.build();
+    }
+}
