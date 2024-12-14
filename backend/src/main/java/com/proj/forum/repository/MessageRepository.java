e5e1a1c1079cbedc82bc03f771331cf0df88b79d
Anton Pampukha <antonpampuha4@gmail.com>
2024-12-14 13:47:53 +0200

    added 2 new service: ChatRoom and Message. Renamed MessageRepository. Create 2 config (copy-past)

--- backend/src/main/java/com/proj/forum/repository/MessageRepository.java
+++ backend/src/main/java/com/proj/forum/repository/MessageRepository.java
@@ -3,9 +3,8 @@ package com.proj.forum.repository;
 import com.proj.forum.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.UUID;
 
-public interface MessangeRepository extends JpaRepository<Message, UUID> {
+public interface MessageRepository extends JpaRepository<Message, UUID> {
 
 }
