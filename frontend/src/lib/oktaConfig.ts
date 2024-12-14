8fd46d3a8543a26c7331142186edadfee7389107
PavloKordunov <102924208+PavloKordunov@users.noreply.github.com>
2024-12-15 01:14:52 +0200

    fix

--- frontend/src/lib/oktaConfig.ts
+++ frontend/src/lib/oktaConfig.ts
@@ -1,7 +1,6 @@
 export const oktaConfig = {
     clientId: '0oalmv1c9zXfKRxLb5d7',
     issuer: 'https://dev-97946146.okta.com/oauth2/default',
-    clientId: '0oalmv1c9zXfKRxLb5d7',
     redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
