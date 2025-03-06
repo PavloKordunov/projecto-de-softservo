79cbac8f715a607d5060a1ebba9ef9b064f74ea3
notvoltyyy <mmsenkiv021@gmail.com>
2025-03-06 20:00:46 +0200

    integrate allure

---
+++ backend/src/test/java/com/proj/forum/service/impl/PostServiceImplTest.java
@@ -0,0 +1,63 @@
+package com.proj.forum.service.impl;
+
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class PostServiceImplTest {
+
+    @BeforeAll
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createPost() {
+    }
+
+    @Test
+    void getAllPosts() {
+    }
+
+    @Test
+    void getPostById() {
+    }
+
+    @Test
+    void getPostsByUser() {
+    }
+
+    @Test
+    void getPostsByGroup() {
+    }
+
+    @Test
+    void updatePost() {
+    }
+
+    @Test
+    void pinPost() {
+    }
+
+    @Test
+    void deletePost() {
+    }
+
+    @Test
+    void mapToPostDtoList() {
+    }
+
+    @Test
+    void isAuthor() {
+    }
+
+    @Test
+    void getByTitleContain() {
+    }
+
+    @Test
+    void addView() {
+    }
+}
