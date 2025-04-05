c6666e34fd261715a468b27b61637db1a0bcf444
Anton Pampukha <antonpampuha4@gmail.com>
2025-04-05 14:10:40 +0300

    fix notifications logic

--- backend/src/main/java/com/proj/forum/config/WebSocketConfig.java
+++ backend/src/main/java/com/proj/forum/config/WebSocketConfig.java
@@ -28,8 +28,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("https://localhost:8080", "https://localhost:3000");
-                //.withSockJS();
+                .setAllowedOrigins("https://localhost:8080", "https://localhost:3000")
+                .withSockJS();
     }
 
 //    @Override
